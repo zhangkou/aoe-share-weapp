676d9e1b38b1461a6418548c4e4071d9dff46b74
kozhang <870478065@qq.com>
2017-05-24 23:58:38 +0800

    .

--- pages/discovery/discovery.js
+++ pages/discovery/discovery.js
@@ -5,19 +5,6 @@ var sizeType = [ ['compressed'], ['original'], ['compressed', 'original'] ]
 
 Page({
   data: {
-    navTab: ["推荐", "圆桌", "热门", "收藏"],
-    currentNavtab: "0",
-    imgUrls: [
-      '../../images/24213.jpg',
-      '../../images/24280.jpg',
-      '../../images/1444983318907-_DSC1826.jpg'
-    ],
-    indicatorDots: false,
-    autoplay: true,
-    interval: 5000,
-    duration: 1000,
-    feed: [],
-    feed_length: 0,
     imageList: [],
     sourceTypeIndex: 2,
     sourceType: ['拍照', '相册', '拍照或相册'],
@@ -30,74 +17,22 @@ Page({
   },
   onLoad: function () {
     console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    this.refresh();
   },
-  switchTab: function(e){
-    this.setData({
-      currentNavtab: e.currentTarget.dataset.idx
+
+  onSubmit: function(event) {
+      wx.showToast({
+        title: "发布成功",
+        success:function(){
+            // 返回首页
+            setTimeout(function(){
+                wx.hideToast();
+                wx.switchTab({
+                  url: '../index/index'
+                })
+            },1000)
+        }
     });
-  },
-
-  bindItemTap: function() {
-    wx.navigateTo({
-      url: '../answer/answer'
-    })
-  },
-  bindQueTap: function() {
-    wx.navigateTo({
-      url: '../question/question'
-    })
-  },
-  upper: function () {
-    wx.showNavigationBarLoading()
-    this.refresh();
-    console.log("upper");
-    setTimeout(function(){wx.hideNavigationBarLoading();wx.stopPullDownRefresh();}, 2000);
-  },
-  lower: function (e) {
-    wx.showNavigationBarLoading();
-    var that = this;
-    setTimeout(function(){wx.hideNavigationBarLoading();that.nextLoad();}, 1000);
-    console.log("lower")
-  },
-  //scroll: function (e) {
-  //  console.log("scroll")
-  //},
-
-  //网络请求数据, 实现刷新
-  refresh0: function(){
-    var index_api = '';
-    util.getData(index_api)
-        .then(function(data){
-          //this.setData({
-          //
-          //});
-          console.log(data);
-        });
-  },
-
-  //使用本地 fake 数据实现刷新效果
-  refresh: function(){
-    var feed = util.getDiscovery();
-    console.log("loaddata");
-    var feed_data = feed.data;
-    this.setData({
-      feed:feed_data,
-      feed_length: feed_data.length
-    });
-  },
-
-  //使用本地 fake 数据实现继续加载效果
-  nextLoad: function(){
-    var next = util.discoveryNext();
-    console.log("continueload");
-    var next_data = next.data;
-    this.setData({
-      feed: this.data.feed.concat(next_data),
-      feed_length: this.data.feed_length + next_data.length
-    });
+    
   },
 
   sourceTypeChange: function (e) {
